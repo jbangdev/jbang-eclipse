9fad310fb00af25aa3b30b6264d4023effa35401
Snjezana Peco <snjezana.peco@redhat.com>
2022-09-21 21:48:37 +0200

    fix: Non-Invisible projects are deleted on workspace startup
    Signed-off-by: Snjezana Peco <snjezana.peco@redhat.com>

--- dev.jbang.eclipse.ls/src/main/java/dev/jbang/eclipse/ls/internal/JBangBuildSupport.java
+++ dev.jbang.eclipse.ls/src/main/java/dev/jbang/eclipse/ls/internal/JBangBuildSupport.java
@@ -1,7 +1,14 @@
 package dev.jbang.eclipse.ls.internal;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.ls.core.internal.JavaLanguageServerPlugin;
 import org.eclipse.jdt.ls.core.internal.managers.IBuildSupport;
 
 import dev.jbang.eclipse.core.internal.JBangFileUtils;
@@ -18,6 +25,28 @@ public class JBangBuildSupport implements IBuildSupport {
 	@Override
 	public boolean isBuildFile(IResource resource) {
 		return JBangFileUtils.isJBangBuildFile(resource);
+	}
+
+	@Override
+	public boolean hasSpecificDeleteProjectLogic() {
+		return true;
+	}
+
+	@Override
+	public void deleteInvalidProjects(Collection<IPath> rootPaths, ArrayList<IProject> deleteProjectCandates, IProgressMonitor monitor) {
+		for (IProject project : deleteProjectCandates) {
+			if (applies(project)) {
+				try {
+					project.getDescription();
+				} catch (CoreException e) {
+					try {
+						project.delete(true, monitor);
+					} catch (CoreException e1) {
+						JavaLanguageServerPlugin.logException(e1.getMessage(), e1);
+					}
+				}
+			}
+		}
 	}
 
 }
